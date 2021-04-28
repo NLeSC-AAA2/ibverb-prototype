189326a79eeb65203236a4b36eeb70276d512015
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2021-04-28 13:19:25 +0200

    Print source IP for packets.

--- udp.c
+++ udp.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200112L
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
 #include <stdio.h>
@@ -22,15 +23,36 @@ void
 server_loop(int sock)
 {
     ssize_t size;
+    struct sockaddr src_addr;
+    socklen_t addrlen = sizeof src_addr;
+
+    char addr_str[INET6_ADDRSTRLEN];
+
     while (packet_loop) {
-        size = recvfrom(sock, udp_buffer, sizeof udp_buffer, 0, NULL, NULL);
+        size = recvfrom(sock, udp_buffer, sizeof udp_buffer, 0, &src_addr, &addrlen);
         if (size == -1) {
             perror("Error receiving packet");
             exit(EXIT_FAILURE);
         }
 
         udp_buffer[size] = '\0';
-        printf("%s\n", udp_buffer);
+
+        switch (src_addr.sa_family) {
+          case AF_INET: {
+              struct sockaddr_in *addr_in = (struct sockaddr_in *) &src_addr;
+              inet_ntop(AF_INET, &addr_in->sin_addr, addr_str, INET_ADDRSTRLEN);
+              break;
+          }
+          case AF_INET6: {
+              struct sockaddr_in6 *addr_in6 = (struct sockaddr_in6 *) &src_addr;
+              inet_ntop(AF_INET6, &addr_in6->sin6_addr, addr_str, INET6_ADDRSTRLEN);
+              break;
+          }
+          default:
+            break;
+        }
+
+        printf("%s: %s\n", addr_str, udp_buffer);
     }
 }
 
