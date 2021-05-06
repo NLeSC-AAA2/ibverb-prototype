1513c119251594139b81e363703689902c166f64
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2021-05-06 15:24:53 +0200

    Cleanup raw UDP implementation.

--- raw.c
+++ raw.c
@@ -3,9 +3,15 @@
  * Licensed under the Apache License, version 2.0. See LICENSE for details.
  */
 
+#define _GNU_SOURCE
+#include <netinet/ether.h>
+#undef _GNU_SOURCE
 #define _POSIX_C_SOURCE 200809L
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <net/ethernet.h>
+#include <net/if.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
@@ -13,6 +19,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <linux/if_packet.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
@@ -25,15 +32,96 @@ void stop_loop(int sig)
     packet_loop = 0;
 }
 
-#define MAX_PACKET_SIZE 65535
-static char ip_buffer[MAX_PACKET_SIZE + 1];
-static struct iphdr *ip_header = (struct iphdr*) ip_buffer;
-static struct udphdr *udp_header = (struct udphdr*) &ip_buffer[sizeof *ip_header];
-static char *udp_data = &ip_buffer[sizeof *ip_header + sizeof *udp_header];
-static const size_t header_size = sizeof *ip_header + sizeof *udp_header;
-static const size_t max_msg_size = MAX_PACKET_SIZE - header_size;
+static char ether_buffer[IP_MAXPACKET];
+static struct ethhdr *ether_header = (struct ethhdr*) ether_buffer;
+static struct iphdr *ip_header = (struct iphdr*) &ether_buffer[sizeof *ether_header];
+static struct udphdr *udp_header = (struct udphdr*) &ether_buffer[sizeof *ether_header + sizeof *ip_header];
+static char *udp_data = &ether_buffer[sizeof *ether_header + sizeof *ip_header + sizeof *udp_header];
+static const size_t header_size = sizeof *ether_header + sizeof *ip_header + sizeof *udp_header;
+static const size_t max_msg_size = IP_MAXPACKET - header_size;
 
-uint16_t ipv4check(char *raw_data, int n)
+struct addr {
+    char mac[ETH_ALEN];
+    uint32_t ipv4;
+};
+
+void
+print_addr(struct addr *remote)
+{
+    struct in_addr addr_ipv4 = { 0 };
+    addr_ipv4.s_addr = remote->ipv4;
+
+    printf("IPv4: %s\n", inet_ntoa(addr_ipv4));
+
+    struct ether_addr addr_eth = { 0 };
+    memcpy(addr_eth.ether_addr_octet, remote->mac, ETH_ALEN);
+
+    printf("MAC: %s\n", ether_ntoa(&addr_eth));
+}
+
+struct addr
+lookup_remote_addr(char *interface, char *host, char *port)
+{
+    struct addr remote = { 0 };
+    struct addrinfo hints = { 0 };
+    struct addrinfo *res;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int result = getaddrinfo(host, port, &hints, &res);
+    if (result != 0) {
+        fprintf(stderr, "Error finding remote adddress: %s\n",
+                gai_strerror(result));
+        exit(EXIT_FAILURE);
+    }
+
+    struct arpreq req = { 0 };
+    memcpy(&req.arp_pa, res->ai_addr, sizeof req.arp_pa);
+    strncpy(req.arp_dev, interface, IFNAMSIZ);
+
+    int sock;
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(sock, SIOCGARP, &req) == -1) {
+        perror("ioctl");
+        exit(EXIT_FAILURE);
+    }
+
+    close(sock);
+
+    memcpy(remote.mac, req.arp_ha.sa_data, ETH_ALEN);
+    remote.ipv4 = ((struct sockaddr_in*) res->ai_addr)->sin_addr.s_addr;
+
+    return remote;
+}
+
+struct addr
+lookup_local_addr(int sock, char *interface)
+{
+    struct ifreq req = { 0 };
+    struct addr local = { 0 };
+
+    strncpy(req.ifr_name, interface, IFNAMSIZ);
+    if (ioctl(sock, SIOCGIFHWADDR, &req) != 0) {
+        perror("failed to lookup MAC address ");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(local.mac, req.ifr_addr.sa_data, ETH_ALEN);
+
+    if (ioctl(sock, SIOCGIFADDR, &req) != 0) {
+        perror("failed to lookup IP address ");
+        exit(EXIT_FAILURE);
+    }
+    local.ipv4 = ((struct sockaddr_in*) &req.ifr_addr)->sin_addr.s_addr;
+
+    return local;
+}
+
+uint16_t ipv4check(void *raw_data, int n)
 {
     uint16_t *data = (uint16_t*) raw_data;
     n = 1 + ((n - 1) / 2);
@@ -63,66 +151,78 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    if (argc != 3) {
+    char *ifname = "eth4";
+    if (argc == 4) {
+        ifname = argv[3];
+    } else if (argc != 3) {
         fprintf(stderr, "Incorrect number of arguments.\n");
         exit(EXIT_FAILURE);
     }
 
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock == -1) {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
-    int msg_size, result;
-    int count = 0;
+    struct addr local = lookup_local_addr(sock, ifname);
+    printf("Local address:\n");
+    print_addr(&local);
 
-    struct addrinfo hints, *res;
+    struct addr remote = lookup_remote_addr(ifname, argv[2], argv[1]);
+    printf("\nRemote address:\n");
+    print_addr(&remote);
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_NUMERICHOST;
-
-    result = getaddrinfo(argv[2], argv[1], &hints, &res);
-    if (result != 0) {
-        fprintf(stderr, "Error finding local adddress: %s\n",
-                gai_strerror(result));
+    struct sockaddr_ll device;
+    memset(&device, 0, sizeof (device));
+    if ((device.sll_ifindex = if_nametoindex(ifname)) == 0) {
+        perror("if_nametoindex() failed to obtain interface index ");
         exit(EXIT_FAILURE);
     }
 
+    device.sll_family = AF_PACKET;
+    device.sll_halen = 6;
+    memcpy(device.sll_addr, remote.mac, ETH_ALEN);
+
+    ether_header->h_proto = htons(0x0800);
+    memcpy(ether_header->h_dest, remote.mac, ETH_ALEN);
+    memcpy(ether_header->h_source, local.mac, ETH_ALEN);
+
+    ip_header->ihl = 5;
+    ip_header->version = 4;
+    ip_header->tos = 0;
+    ip_header->id = htonl (54321);
+    ip_header->frag_off = 0;
+    ip_header->ttl = 255;
+    ip_header->protocol = IPPROTO_UDP;
+    ip_header->check = 0;
+    ip_header->daddr = remote.ipv4;
+    ip_header->saddr = local.ipv4;
+
+    udp_header->dest = htons(atoi(argv[1]));
+    udp_header->source = htons(4242);
+
+    int count = 0;
     while (packet_loop) {
-        msg_size = snprintf(udp_data, max_msg_size, "Message: %d", count++);
+        int msg_size = snprintf(udp_data, max_msg_size, "Message: %d", count++);
         if (msg_size < 0 || (size_t) msg_size >= max_msg_size) {
             perror("Error creating message");
-            freeaddrinfo(res);
             exit(EXIT_FAILURE);
         }
 
         uint16_t total_length = header_size + msg_size;
-        ip_header->ihl = 5;
-        ip_header->version = 4;
-        ip_header->tos = 0;
-        ip_header->tot_len = htons(total_length);
-        ip_header->id = htonl (54321);
-        ip_header->frag_off = 0;
-        ip_header->ttl = 255;
-        ip_header->protocol = IPPROTO_UDP;
-        ip_header->check = 0;
-        ip_header->saddr = inet_addr("10.141.0.104");
-        ip_header->daddr = ((struct sockaddr_in*) res->ai_addr)->sin_addr.s_addr;
+        uint16_t ip_length = total_length - sizeof *ether_header;
 
-        udp_header->source = htons(4242);
-        udp_header->dest = htons(atoi(argv[1]));
+        ip_header->tot_len = htons(ip_length);
+
         udp_header->len = htons(8 + msg_size);
         udp_header->check = 0;
 
-        ip_header->check = ipv4check(ip_buffer, total_length);
+        ip_header->check = ipv4check(ip_header, sizeof *ip_header);
 
-        result = sendto(sock, ip_buffer, total_length, 0, res->ai_addr, res->ai_addrlen);
+        int result = sendto(sock, ether_buffer, total_length, 0, (struct sockaddr *) &device, sizeof (device));
         if (result == -1) {
             perror("Error sending message");
-            freeaddrinfo(res);
             exit(EXIT_FAILURE);
         }
         sleep(1);
