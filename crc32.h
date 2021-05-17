63db4eefc24139e04ae1d9d02635181bc6d99db7
Merijn Verstraaten <m.verstraaten@esciencecenter.nl>
2021-05-17 15:39:05 +0200

    Add missing include guards.

--- crc32.h
+++ crc32.h
@@ -3,7 +3,11 @@
  * Licensed under the Apache License, version 2.0. See LICENSE for details.
  */
 
+#ifndef CRC32_H
+#define CRC32_H
+
 #include <stdint.h>
 #include <unistd.h>
 
 uint32_t crc32(uint32_t crc, const unsigned char *data, ssize_t size);
+#endif
--- crc32table.h
+++ crc32table.h
@@ -3,6 +3,9 @@
  * Licensed under the Apache License, version 2.0. See LICENSE for details.
  */
 
+#ifndef CRC32TABLE_H
+#define CRC32TABLE_H
+
 uint32_t crc32table[] = {
 0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA,
 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
@@ -69,3 +72,4 @@ uint32_t crc32table[] = {
 0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94,
 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D
 };
+#endif
